043c504f6a4722fd1a62caa120b8531c4593eb16
Mikael Fourré <fmikl@outlook.fr>
2026-05-14 09:16:08 +0200

    Wire additional validators

--- src/core/FormGuard.js
+++ src/core/FormGuard.js
@@ -1,8 +1,17 @@
 import FieldValidator from './FieldValidator.js';
+import CheckboxValidator from '../validators/CheckboxValidator.js';
+import ConfirmationValidator from '../validators/ConfirmationValidator.js';
+import DateValidator from '../validators/DateValidator.js';
 import EmailValidator from '../validators/EmailValidator.js';
+import FileValidator from '../validators/FileValidator.js';
+import NumberValidator from '../validators/NumberValidator.js';
 import PasswordValidator from '../validators/PasswordValidator.js';
 import PhoneValidator from '../validators/PhoneValidator.js';
+import RadioValidator from '../validators/RadioValidator.js';
+import RequiredValidator from '../validators/RequiredValidator.js';
+import SelectValidator from '../validators/SelectValidator.js';
 import TextValidator from '../validators/TextValidator.js';
+import UrlValidator from '../validators/UrlValidator.js';
 
 export default class FormGuard {
     static init() {
@@ -19,8 +28,23 @@ export default class FormGuard {
                         let validator;
 
                         switch (type) {
+                            case 'checkbox':
+                                validator = new CheckboxValidator(field);
+                                break;
+                            case 'confirmation':
+                                validator = new ConfirmationValidator(field);
+                                break;
+                            case 'date':
+                                validator = new DateValidator(field);
+                                break;
                             case 'email':
                                 validator = new EmailValidator(field);
+                                break;
+                            case 'file':
+                                validator = new FileValidator(field);
+                                break;
+                            case 'number':
+                                validator = new NumberValidator(field);
                                 break;
                             case 'password':
                                 validator = new PasswordValidator(field);
@@ -28,8 +52,20 @@ export default class FormGuard {
                             case 'phone':
                                 validator = new PhoneValidator(field);
                                 break;
+                            case 'radio':
+                                validator = new RadioValidator(field);
+                                break;
+                            case 'required':
+                                validator = new RequiredValidator(field);
+                                break;
+                            case 'select':
+                                validator = new SelectValidator(field);
+                                break;
                             case 'text':
                                 validator = new TextValidator(field);
+                                break;
+                            case 'url':
+                                validator = new UrlValidator(field);
                                 break;
                             default:
                                 return;
@@ -49,10 +85,13 @@ export default class FormGuard {
                 });
 
                 form.querySelectorAll('[data-validate]').forEach(field => {
-                    field.addEventListener('input', () => {
+                    const clearFieldError = () => {
                         const validator = new FieldValidator(field);
                         validator.clearError();
-                    });
+                    };
+
+                    field.addEventListener('input', clearFieldError);
+                    field.addEventListener('change', clearFieldError);
                 });
             });
         });
